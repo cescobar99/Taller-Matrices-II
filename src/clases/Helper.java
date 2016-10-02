2bddb421060f34037fb8da66db76b466c16b74d8
“Camilo <camiloelrey2014@gmail.com>
2016-10-01 19:40:08 -0500

    Faltan solo figuras. letras terminadas y validadas en general

--- src/clases/Helper.java
+++ src/clases/Helper.java
@@ -89,7 +89,7 @@ public class Helper {
         for (int i = 0; i < nf; i++) {
             for (int j = 0; j < nc; j++) {
                 aux = (int) tabla1.getValueAt(i, j);
-                if ((j == 0) || (i == j + 2) || (nf - 1 - i == j + 2)) {
+                if ((i+(j-1)==(nf-1)/2)||(i-(j-1)==(nf-1)/2)||(j==0)) {
                     tabla2.setValueAt(aux, i, j);
                 }
             }
